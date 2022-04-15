189218b20641177356363535bee49e9a67db8572
Jimmy Song <rootsongjc@gmail.com>
2022-04-15 16:26:11 +0800

    remove node modules

--- node_modules/prettier-standard/lib/no-main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-/* eslint no-console:0 */
-var message = "\nLooks like you're trying to require/import `prettier-standard`.\nThis module doesn't actually expose a NodeJS interface.\nThis module's just the CLI for `prettier-standard`.\n".trim();
-
-console.info(message);
-
-module.exports = message;
